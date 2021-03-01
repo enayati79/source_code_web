10c40171dcc3f5240a1291b8a788dba6aefe2a5a
enayati79 <79705811+enayati79@users.noreply.github.com>
2021-03-01 10:57:33 +0330

    Add files via upload

---
+++ 3.2.0/action.php
@@ -0,0 +1,14 @@
+<?php
+var_dump($_POST);
+var_dump($_GET);
+var_dump( $_REQUEST ); 
+ 
+ $dbc = new mysqli('sql200.gigfa.com', 'gigfa_25765222', 'sng2hfv4', 'gigfa_25765222_815'); 
+  $sql = "INSERT INTO cm (title, body)
+  VALUES('{$_POST['title']}', '{$_POST['body']}')"; 
+  echo $sql. '<br>';
+   $result = $dbc->query( $sql ); 
+   $dbc->close();
+   echo'با موفقیت ثبت شد'
+  
+?>
